8828b93accde1dfcab4d249bcd495c74511bece2
shadowvall <shadowvall@1bee7092-f547-41a6-aea2-8cc0afc07e7d>
2010-10-07 18:26:57 +0000

    added versions and comments to some files

--- SearcherPawn.lua
+++ SearcherPawn.lua
@@ -1,6 +1,6 @@
 ﻿--[[
   Auctioneer Advanced - Search UI - Searcher Pawn
-  Version: 1.1.7 (Xit)
+  Version: 1.1.10 (Xit & Shadowvall)
   Revision: $Id: SearcherPawn.lua 1.1.7 20090920 Xit $
   URL: http://wow.curse.com/downloads/wow-addons/details/auc-advanced-searcher-pawn.aspx
 
