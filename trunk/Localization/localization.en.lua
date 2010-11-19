981c196ae88a784085e18f660d51e994d76e041c
xit_draka <xit_draka@1bee7092-f547-41a6-aea2-8cc0afc07e7d>
2010-11-19 04:54:50 +0000

    cleaned up code.  Fixed comparison of off hand items when you have a 2h item equipped.

--- trunk/Localization/localization.en.lua
+++ trunk/Localization/localization.en.lua
@@ -31,18 +31,17 @@ Localization.RegisterAddonStrings("enUS", "Auc-Searcher-Pawn",
     HELP_QUESTION     = "What does this searcher do?";
     HELP_ANSWER       = "This searcher will use a scale that has been defined in the Pawn addon to locate upgrades for your currently equipped items.";
     CONFIG_HEADER     = "Pawn Search Criteria";
-    SCALE_SELECT      = "Pawn scale name";
+    SCALE_SELECT      = "Pawn scale";
     SCALE_SELECT_TIP  = "The Pawn scale that will be used to determine the item value";
-    CHECK_CURRENT     = "Check current value";
-    CHECK_CURRENT_TIP = "If checked, items will be calculated 'as is'.  Empty sockets will be ignored.  All enchants applied.  If unchecked, items will be calculated without any enchantments and all empty sockets will use the value you set in the Pawn scale.";
     USEABLE_ONLY      = "Useable items only";
     USEABLE_ONLY_TIP  = "Only items that your character can use.";
-    FORCE_MAIN        = "Force main hand";
-    FORCE_MAIN_TIP    = "Off-hand items will only be compared to your main hand slot.";
     AFFORD_ONLY       = "Only what I can afford";
     AFFORD_ONLY_TIP   = "Only show what you can currently afford to buy.";
     USE_BUYOUT        = "Use buyout";
     USE_BUYOUT_TIP    = "Use buyout instead of bid when checking auction prices.";
+    FORCE2H_WEAP      = "Only 2H Weapons";
+    FORCE2H_TIP       = "When comparing weapons, only consider 2-Handed Weapons.";
+
     INCLUDE_IN_SEARCH = "Include these slots when searching:";
     SHOW_HEAD         = "Head";
     SHOW_NECK         = "Neck";
@@ -81,40 +80,23 @@ Localization.RegisterAddonStrings("enUS", "Auc-Searcher-Pawn",
     SIGILS  = "Sigils";
 
     -- Messages
-    NO_SCALE     = "No scale name";
     NO_SCALE_DBG = "DBG: No scale defined, please pick a scale from the dropdown list.  If none exist you will need to define a scale in Pawn first.";
 
-    SCALE_BLANK     = "Scale left blank";
     SCALE_BLANK_DBG = "DBG: No scale defined, please pick a scale from the dropdown list.  If none exist you will need to define a scale in Pawn first.";
 
-    SCALE_NOT_FOUND     = "Scale not found";
     SCALE_NOT_FOUND_DBG = "DBG: Error! Pawn cannot find scale";
 
-    NO_LINK     = "No link";
     NO_LINK_DBG = "DBG: No link";
 
-    CANNOT_USE     = "Can't use";
     CANNOT_USE_DBG = "DBG: Can't use this item";
 
-    NOT_EQUIPPABLE     = "Not Equippable";
-    NOT_EQUIPPABLE_DBG = "DBG: This item is not equippable";
-
-    CANNOT_AFFORD_BUYOUT = "Can't afford buyout";
-    CANNOT_AFFORD_BID    = "Can't afford bid";
-    CANNOT_AFFORD_DBG    = "DBG: Can't afford this item";
-
     NOT_WANTED     = "Item slot not wanted";
-    NOT_WANTED_DBG = "DBG: Item slot not wanted";
 
     REASON_BUY = "buy";
     REASON_BID = "bid";
 
-    NO_ITEM     = "No item";
     NO_ITEM_DBG = "DBG: Pawn has no data for this item";
 
-    EMPTY_SLOT = "Empty Slot";
-
     NOT_UPGRADE     = "Pawn value is too low.";
-    NOT_UPGRADE_DBG = "DBG: Not an upgrade.  Pawn value is too low";
   },
 nil, true) -- Protected
