f877cd4af78fb68e49cd1ff052854c68573cf91b
xit_draka <xit_draka@1bee7092-f547-41a6-aea2-8cc0afc07e7d>
2011-12-06 05:47:02 +0000

    updated ptBR translations.

--- Localization/localization.pt.lua
+++ Localization/localization.pt.lua
@@ -3,8 +3,8 @@
 Localization.RegisterAddonStrings("ptBR", "Localization",
   {
     -- Locales
-    enUS      = "Inglês";
-    enGB      = "Inglês";
+    enUS      = "Inglês Americano";
+    enGB      = "Inglês Europeu";
     frFR      = "Francês";
     deDE      = "Alemão";
     ruRU      = "Russo";
@@ -15,7 +15,7 @@ Localization.RegisterAddonStrings("ptBR", "Localization",
     Confirm         = "Confirmar";
     Cancel          = "Cancelar";
     RemoveUnused    = "Remover da memoria localizações não usadas.";
-    SelectPreferred = "Seleccione um o idioma preferido:";
+    SelectPreferred = "Seleccione o idioma preferido:";
     AvailLangs      = "Idiomas disponíveis";
 
     --Earth Button
@@ -28,25 +28,25 @@ nil, true) -- Protected
 Localization.RegisterAddonStrings("ptBR", "Auc-Searcher-Pawn",
   {
     -- Config
-    MAIN_TITLE        = "Procurar for itens que o Pawn considera um melhoramento";
+    MAIN_TITLE        = "Procurar por itens que o Pawn considera um upgrade";
     OPTIONS           = "Opções:";
     HELP_ID           = "Pesquisador do Pawn";
     HELP_QUESTION     = "O que faz este pesquisador?";
     HELP_ANSWER       = "Esta procura irá usar a escala que foi definida no addon Pawn para localizar upgrades para o equipamento que tem equipado.";
     CONFIG_HEADER     = "Critérios de procura do Pawn";
     SCALE_SELECT      = "Escala do Pawn";
-    SCALE_SELECT_TIP  = "A escala do Pawn que que vai ser usada para determinar o valor do item";
+    SCALE_SELECT_TIP  = "A escala do Pawn que vai ser usada para determinar o valor do item";
     USEABLE_ONLY      = "Apenas itens que possa usar";
     USEABLE_ONLY_TIP  = "Apenas itens que o seu personagem possa usar.";
     AFFORD_ONLY       = "Apenas o que consigo comprar";
     AFFORD_ONLY_TIP   = "Mostra apenas o que consegue comprar de momento com o dinheiro disponível.";
     USE_BUYOUT        = "Usar Arremate";
-    USE_BUYOUT_TIP    = "Usar Arremate em vez de Lance quando verificar os preços dos leilões.";
-    USE_BESTPRICE     = "Ajustar o 'score' baseado no preço.";
-    USE_BESTPRICE_TIP = "Ajustar o 'score' mostrado pelo preço do item. Para itens similares, o mais barato ficará acima na lista.";
-    FORCE2H_WEAP      = "Apenas Armas 2M";
+    USE_BUYOUT_TIP    = "Usar Arremate em vez de Dar lance quando verificar os preços dos leilões.";
+    USE_BESTPRICE     = "Ajustar o valor baseado no preço.";
+    USE_BESTPRICE_TIP = "Ajustar pelo preço do item o valor mostrado. Para itens similares, o mais barato ficará acima na lista.";
+    FORCE2H_WEAP      = "Apenas de 2M";
     FORCE2H_TIP       = "Ao comparar armas, considerar apenas Armas de Duas mãos";
-    INCLUDE_IN_SEARCH = "Incluir estas caixas ao procurar:";
+    INCLUDE_IN_SEARCH = "Incluir estes compartimentos ao procurar:";
     SHOW_HEAD         = "Cabeça";
     SHOW_NECK         = "Pescoço";
     SHOW_SHOULDER     = "Ombro";
@@ -60,11 +60,11 @@ Localization.RegisterAddonStrings("ptBR", "Auc-Searcher-Pawn",
     SHOW_FINGER       = "Dedo";
     SHOW_TRINKET      = "Berloque";
     SHOW_WEAPON       = "Arma";
-    SHOW_OFFHAND      = "Mão Secundária";
+    SHOW_OFFHAND      = "Mão secundária";
     SHOW_RANGED       = "Arremesso";
 
     --Classes
-    WARRIOR      = "Gerreiro";
+    WARRIOR      = "Guerreiro";
     HUNTER       = "Caçador";
     SHAMAN       = "Shamã";
     ROGUE        = "Ladino";
@@ -72,22 +72,22 @@ Localization.RegisterAddonStrings("ptBR", "Auc-Searcher-Pawn",
 
     --Item Types
     ARMOR   = "Armadura";
-    TOTEMS  = "Totems";
-    LIBRAMS = "Librams";
+    TOTEMS  = "Totens";
+    LIBRAMS = "Incunábulos";
     IDOLS   = "Ídolos";
-    SIGILS  = "Sigils";
+    SIGILS  = "Signos";
 
     --Two Handed substr
     TWOHAND = "Duas Mãos";  -- used to check if user has a 2handed weapon on or not
     STAVES = "Cajados";     -- used to check if user has a 2handed weapon on or not
-    POLEARMS = "Arma de Haste"; -- used to check if user has a 2handed weapon on or not
+    POLEARMS = "Armas de Haste"; -- used to check if user has a 2handed weapon on or not
 
     -- Skill
-    DUALWIELD = "Dual Wield";  -- used to see if a hunter has learned the ability or not
+    DUALWIELD = "Duas Armas";  -- used to see if a hunter has learned the ability or not
 
     -- Messages
     BAD_SCALE   = "Por favor seleccione uma escala válida.";
-    NOT_WANTED  = "Caixa de item não incluída";
+    NOT_WANTED  = "Compartimento de item não incluído";
     NOT_UPGRADE = "O valor do Pawn é muito baixo.";
 
     REASON_BUY = "comprar";
