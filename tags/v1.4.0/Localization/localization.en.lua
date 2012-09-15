03529b4bc05af142fdade4f734a8037371f487c4
xit_draka <xit_draka@1bee7092-f547-41a6-aea2-8cc0afc07e7d>
2012-09-15 19:44:50 +0000

    Tagging as v1.4.0

---
+++ tags/v1.4.0/Localization/localization.en.lua
@@ -0,0 +1,105 @@
+-- English text strings
+
+Localization.RegisterAddonStrings("enUS", "Localization",
+  {
+    -- Locales
+    enUS      = "English";
+    enGB      = "English";
+    frFR      = "French";
+    ruRU      = "Russian";
+    zhTW      = "Traditional Chinese";
+    deDE      = "German";
+    ptBR      = "Brazilian Portuguese";
+
+    -- Frame Strings
+    Confirm         = "Confirm";
+    Cancel          = "Cancel";
+    RemoveUnused    = "Remove unused locales from memory.";
+    SelectPreferred = "Select a preferred localization:";
+    AvailLangs      = "Available Languages";
+
+    --Earth Button
+    Localization  = "Localization";
+    ShowPrompt    = "Show Prompt";
+    EarthTooltip  = "Show Locale Selection Dialogue."
+  },
+nil, true) -- Protected
+
+Localization.RegisterAddonStrings("enUS", "Auc-Searcher-Pawn",
+  {
+    -- Config
+    MAIN_TITLE        = "Search for items which Pawn considers an upgrade";
+    OPTIONS           = "Options:";
+    HELP_ID           = "Pawn Searcher";
+    HELP_QUESTION     = "What does this searcher do?";
+    HELP_ANSWER       = "This searcher will use a scale that has been defined in the Pawn addon to locate upgrades for your currently equipped items.";
+    CONFIG_HEADER     = "Pawn Search Criteria";
+    SCALE_SELECT      = "Pawn scale";
+    SCALE_SELECT_TIP  = "The Pawn scale that will be used to determine the item value";
+    USEABLE_ONLY      = "Useable items only";
+    USEABLE_ONLY_TIP  = "Only items that your character can use.";
+    AFFORD_ONLY       = "Only what I can afford";
+    AFFORD_ONLY_TIP   = "Only show what you can currently afford to buy.";
+    USE_BUYOUT        = "Use buyout";
+    USE_BUYOUT_TIP    = "Use buyout instead of bid when checking auction prices.";
+    USE_BESTPRICE        = "Adjust score based on price.";
+    USE_BESTPRICE_TIP    = "Adjust the score returned by the price of the item.  For similar items, the cheaper item will be higher on the list.";
+    FORCE2H_WEAP      = "Only 2H Weapons";
+    FORCE2H_TIP       = "When comparing weapons, only consider 2-Handed Weapons.";
+    INCLUDE_IN_SEARCH = "Include these slots when searching:";
+    SHOW_HEAD         = "Head";
+    SHOW_NECK         = "Neck";
+    SHOW_SHOULDER     = "Shoulder";
+    SHOW_BACK         = "Back";
+    SHOW_CHEST        = "Chest";
+    SHOW_WRIST        = "Wrist";
+    SHOW_HANDS        = "Hands";
+    SHOW_WAIST        = "Waist";
+    SHOW_LEGS         = "Legs";
+    SHOW_FEET         = "Feet";
+    SHOW_FINGER       = "Finger";
+    SHOW_TRINKET      = "Trinket";
+    SHOW_WEAPON       = "Weapon";
+    SHOW_OFFHAND      = "Off-Hand";
+    SHOW_RANGED       = "Ranged";
+
+    --Classes
+    WARRIOR      = "Warrior";
+    HUNTER       = "Hunter";
+    SHAMAN       = "Shaman";
+    ROGUE        = "Rogue";
+    DEATH_KNIGHT = "Death Knight";
+
+    --Item Types
+    ARMOR   = "Armor";
+    TOTEMS  = "Totems";
+    LIBRAMS = "Librams";
+    IDOLS   = "Idols";
+    SIGILS  = "Sigils";
+
+    --Two Handed substr
+    TWOHAND = "Two-Hand";  -- used to check if user has a 2handed weapon on or not
+    STAVES = "Staves";     -- used to check if user has a 2handed weapon on or not
+    POLEARMS = "Polearms"; -- used to check if user has a 2handed weapon on or not
+    RANGED = "Ranged";     -- used to check if user has a 2handed weapon on or not
+
+    BOWS = "Bows"; -- used to check if a user wants to evaluate Ranged weapons
+    GUNS = "Guns"; -- used to check if a user wants to evaluate Ranged weapons
+    WANDS = "Wands"; -- used to check if a user wants to evaluate Ranged weapons
+    CROSSBOWS = "Crossbows"; -- used to check if a user wants to evaluate Ranged weapons
+    THROWN = "Thrown"; -- used to check if a user wants to evaluate Ranged weapons
+
+    -- Skill
+    DUALWIELD = "Dual Wield";  -- used to see if a shaman or hunter has learned the ability or not
+    CRAZED_BERZERKER = "Crazed Berzerker"; -- skill that gives warriors dual wield
+    TITANS_GRIP = "Titan's Grip"; -- skill that gives warriors ability to dual wield 2handed weapons
+
+    -- Messages
+    BAD_SCALE   = "Please select a valid scale.";
+    NOT_WANTED  = "Item slot not wanted";
+    NOT_UPGRADE = "Pawn value is too low.";
+
+    REASON_BUY = "buy";
+    REASON_BID = "bid";
+  },
+nil, true) -- Protected
---
+++ tags/v1.4.0/Localization/localization.pt.lua
@@ -0,0 +1,96 @@
+-- English text strings
+
+Localization.RegisterAddonStrings("ptBR", "Localization",
+  {
+    -- Locales
+    enUS      = "Inglês Americano";
+    enGB      = "Inglês Europeu";
+    frFR      = "Francês";
+    deDE      = "Alemão";
+    ruRU      = "Russo";
+    zhTW      = "Chinês Tradicional";
+    ptBR      = "Português";
+
+    -- Frame Strings
+    Confirm         = "Confirmar";
+    Cancel          = "Cancelar";
+    RemoveUnused    = "Remover da memoria localizações não usadas.";
+    SelectPreferred = "Seleccione o idioma preferido:";
+    AvailLangs      = "Idiomas disponíveis";
+
+    --Earth Button
+    Localization  = "Localização";
+    ShowPrompt    = "Mostrar aviso";
+    EarthTooltip  = "Mostrar dialogo de selecção local."
+  },
+nil, true) -- Protected
+
+Localization.RegisterAddonStrings("ptBR", "Auc-Searcher-Pawn",
+  {
+    -- Config
+    MAIN_TITLE        = "Procurar por itens que o Pawn considera um upgrade";
+    OPTIONS           = "Opções:";
+    HELP_ID           = "Pesquisador do Pawn";
+    HELP_QUESTION     = "O que faz este pesquisador?";
+    HELP_ANSWER       = "Esta procura irá usar a escala que foi definida no addon Pawn para localizar upgrades para o equipamento que tem equipado.";
+    CONFIG_HEADER     = "Critérios de procura do Pawn";
+    SCALE_SELECT      = "Escala do Pawn";
+    SCALE_SELECT_TIP  = "A escala do Pawn que vai ser usada para determinar o valor do item";
+    USEABLE_ONLY      = "Apenas itens que possa usar";
+    USEABLE_ONLY_TIP  = "Apenas itens que o seu personagem possa usar.";
+    AFFORD_ONLY       = "Apenas o que consigo comprar";
+    AFFORD_ONLY_TIP   = "Mostra apenas o que consegue comprar de momento com o dinheiro disponível.";
+    USE_BUYOUT        = "Usar Arremate";
+    USE_BUYOUT_TIP    = "Usar Arremate em vez de Dar lance quando verificar os preços dos leilões.";
+    USE_BESTPRICE     = "Ajustar o valor baseado no preço.";
+    USE_BESTPRICE_TIP = "Ajustar pelo preço do item o valor mostrado. Para itens similares, o mais barato ficará acima na lista.";
+    FORCE2H_WEAP      = "Apenas de 2M";
+    FORCE2H_TIP       = "Ao comparar armas, considerar apenas Armas de Duas mãos";
+    INCLUDE_IN_SEARCH = "Incluir estes compartimentos ao procurar:";
+    SHOW_HEAD         = "Cabeça";
+    SHOW_NECK         = "Pescoço";
+    SHOW_SHOULDER     = "Ombro";
+    SHOW_BACK         = "Costas";
+    SHOW_CHEST        = "Torso";
+    SHOW_WRIST        = "Pulsos";
+    SHOW_HANDS        = "Mãos";
+    SHOW_WAIST        = "Cintura";
+    SHOW_LEGS         = "Pernas";
+    SHOW_FEET         = "Pés";
+    SHOW_FINGER       = "Dedo";
+    SHOW_TRINKET      = "Berloque";
+    SHOW_WEAPON       = "Arma";
+    SHOW_OFFHAND      = "Mão secundária";
+    SHOW_RANGED       = "Arremesso";
+
+    --Classes
+    WARRIOR      = "Guerreiro";
+    HUNTER       = "Caçador";
+    SHAMAN       = "Shamã";
+    ROGUE        = "Ladino";
+    DEATH_KNIGHT = "Cavaleiro da Morte";
+
+    --Item Types
+    ARMOR   = "Armadura";
+    TOTEMS  = "Totens";
+    LIBRAMS = "Incunábulos";
+    IDOLS   = "Ídolos";
+    SIGILS  = "Signos";
+
+    --Two Handed substr
+    TWOHAND = "Duas Mãos";  -- used to check if user has a 2handed weapon on or not
+    STAVES = "Cajados";     -- used to check if user has a 2handed weapon on or not
+    POLEARMS = "Armas de Haste"; -- used to check if user has a 2handed weapon on or not
+
+    -- Skill
+    DUALWIELD = "Duas Armas";  -- used to see if a hunter has learned the ability or not
+
+    -- Messages
+    BAD_SCALE   = "Por favor seleccione uma escala válida.";
+    NOT_WANTED  = "Compartimento de item não incluído";
+    NOT_UPGRADE = "O valor do Pawn é muito baixo.";
+
+    REASON_BUY = "comprar";
+    REASON_BID = "lance";
+  },
+nil, true) -- Protected
